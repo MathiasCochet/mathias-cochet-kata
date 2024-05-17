7e41f683455f4d250ffd51ec50c246c79fdb2978
Cochet <Mathias.Cochet@rmdy.be>
2024-05-17 16:14:31 +0200

    using ranges instead

--- src/main/kotlin/com/gildedrose/itemupdaters/EventItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/EventItemUpdater.kt
@@ -6,8 +6,8 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 class EventItemUpdater : BaseItemUpdater() {
 
     companion object {
-        private const val EVENT_NEAR = 10
-        private const val EVENT_VERY_NEAR = 5
+        private val eventVeryNearRange = IntRange(0, 4)
+        private val eventNearRange = IntRange(5, 9)
 
         private const val NORMAL_INCREMENT = 1
         private const val EVENT_NEAR_INCREMENT = 2
@@ -28,9 +28,9 @@ class EventItemUpdater : BaseItemUpdater() {
     private fun getIncrementValue(sellin: Int): Int? {
         if (sellin < 0) return null;
 
-        return when {
-            sellin < EVENT_VERY_NEAR -> EVENT_VERY_NEAR_INCREMENT
-            sellin < EVENT_NEAR -> EVENT_NEAR_INCREMENT
+        return when (sellin) {
+            in eventVeryNearRange -> EVENT_VERY_NEAR_INCREMENT
+            in eventNearRange -> EVENT_NEAR_INCREMENT
             else -> NORMAL_INCREMENT
         }
     }
