8517e7daf3c6f4efc29c2ace7bcb6db6d185ccfa
Cochet <Mathias.Cochet@rmdy.be>
2024-05-17 16:29:38 +0200

    Removed legacy code and cleaned up new code.

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,75 +1,12 @@
 package com.gildedrose
 
-import com.gildedrose.itemupdaters.*
-import com.gildedrose.itemupdaters.base.ItemUpdater
 import com.gildedrose.itemupdaters.base.getItemUpdater
 
 class GildedRose(private var items: List<Item>) {
 
-    fun updateQuality() {
-        for (i in items.indices) {
-            val item = items[i]
-            val itemUpdater: ItemUpdater = item.getItemUpdater()
-
-            if (itemUpdater is DefaultItemUpdater
-                || itemUpdater is AgedItemUpdater
-                || itemUpdater is DwindlingItemUpdater
-                || itemUpdater is EventItemUpdater
-                || itemUpdater is ImmutableItemUpdater
-            ) {
-                itemUpdater.updateItem(item)
-            } else {
-                //Legacy code
-                if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (items[i].quality > 0) {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                            items[i].quality = items[i].quality - 1
-                        }
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-
-                        if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                            if (items[i].sellIn < 11) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1
-                                }
-                            }
-
-                            if (items[i].sellIn < 6) {
-                                if (items[i].quality < 50) {
-                                    items[i].quality = items[i].quality + 1
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                    items[i].sellIn = items[i].sellIn - 1
-                }
-
-                if (items[i].sellIn < 0) {
-                    if (items[i].name != "Aged Brie") {
-                        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                            if (items[i].quality > 0) {
-                                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                    items[i].quality = items[i].quality - 1
-                                }
-                            }
-                        } else {
-                            items[i].quality = items[i].quality - items[i].quality
-                        }
-                    } else {
-                        if (items[i].quality < 50) {
-                            items[i].quality = items[i].quality + 1
-                        }
-                    }
-                }
-            }
-        }
+    fun updateQuality() = items.forEach { item ->
+        val itemUpdater = item.getItemUpdater()
+        itemUpdater.updateItem(item)
     }
-
 }
 
