03ebddbbbc9e16d362cce60a373c1e46804439b2
mCochet <mathias_cochet@hotmail.com>
2024-05-17 16:15:21 +0200

    Feature/event item updater (#5)
    * event item updater test added
    
    * event item updater logic added
    
    * event item updater added to gilded rose application
    
    * using ranges instead
    
    ---------
    
    Co-authored-by: Cochet <Mathias.Cochet@rmdy.be>

--- src/main/kotlin/com/gildedrose/itemupdaters/EventItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/EventItemUpdater.kt
@@ -5,7 +5,33 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 
 class EventItemUpdater : BaseItemUpdater() {
 
+    companion object {
+        private val eventVeryNearRange = IntRange(0, 4)
+        private val eventNearRange = IntRange(5, 9)
+
+        private const val NORMAL_INCREMENT = 1
+        private const val EVENT_NEAR_INCREMENT = 2
+        private const val EVENT_VERY_NEAR_INCREMENT = 3
+    }
+
     override fun updateItem(item: Item) {
         super.updateItem(item)
+
+        item.apply {
+            quality = getIncrementValue(sellIn)
+                ?.let { minOf(quality + it, MAXIMUM_QUALITY_VALUE) }
+                ?: MINIMUM_QUALITY_VALUE
+        }
+
+    }
+
+    private fun getIncrementValue(sellin: Int): Int? {
+        if (sellin < 0) return null;
+
+        return when (sellin) {
+            in eventVeryNearRange -> EVENT_VERY_NEAR_INCREMENT
+            in eventNearRange -> EVENT_NEAR_INCREMENT
+            else -> NORMAL_INCREMENT
+        }
     }
 }
