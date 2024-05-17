ef7e158fa5b9386670bc723709b5194c5aad75a9
Cochet <Mathias.Cochet@rmdy.be>
2024-05-17 14:26:53 +0200

    default item updater logic added

--- src/main/kotlin/com/gildedrose/itemupdaters/DefaultItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/DefaultItemUpdater.kt
@@ -5,7 +5,23 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 
 class DefaultItemUpdater : BaseItemUpdater() {
 
+    companion object {
+        private const val NORMAL_DECREMENT = 1
+        private const val EXPIRED_DECREMENT = 2
+    }
+
     override fun updateItem(item: Item) {
         super.updateItem(item)
+
+        item.apply {
+            quality = maxOf(
+                quality - getDecrementValue(sellIn),
+                MINIMUM_QUALITY_VALUE,
+            )
+        }
+    }
+
+    private fun getDecrementValue(sellin: Int): Int {
+        return if (sellin >= 0) NORMAL_DECREMENT else EXPIRED_DECREMENT
     }
 }
