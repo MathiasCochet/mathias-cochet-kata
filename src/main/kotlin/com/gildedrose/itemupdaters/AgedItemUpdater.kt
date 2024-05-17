3ae7fb5c0fc989474b385ab25ca55b47dbfe16d0
Cochet <Mathias.Cochet@rmdy.be>
2024-05-17 15:10:03 +0200

    aged item updater logic added

--- src/main/kotlin/com/gildedrose/itemupdaters/AgedItemUpdater.kt
+++ src/main/kotlin/com/gildedrose/itemupdaters/AgedItemUpdater.kt
@@ -5,7 +5,23 @@ import com.gildedrose.itemupdaters.base.BaseItemUpdater
 
 class AgedItemUpdater : BaseItemUpdater() {
 
+    companion object {
+        private const val NORMAL_INCREMENT = 1
+        private const val EXPIRED_INCREMENT = 2
+    }
+
     override fun updateItem(item: Item) {
         super.updateItem(item)
+
+        item.apply {
+            quality = minOf(
+                quality + getIncrementValue(sellIn),
+                MAXIMUM_QUALITY_VALUE,
+            )
+        }
+    }
+
+    private fun getIncrementValue(sellin: Int): Int {
+        return if (sellin >= 0) NORMAL_INCREMENT else EXPIRED_INCREMENT
     }
 }
